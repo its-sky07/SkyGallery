229de55870b733838b7e0040d8d4400999ec6477
Akash yadav <149149650+its-sky07@users.noreply.github.com>
2024-08-09 14:58:08 +0530

    Update server.js
    same

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ dotenv.config()
 const app = express()
 app.use(cookieParser())
 app.use(cors({
-  origin: 'https://slygallery.vercel.app/', // Don't use wildcard '*' instead specify frontend url
+  origin: 'https://slygallery.vercel.app', // Don't use wildcard '*' instead specify frontend url
   credentials: true
 }))
 
