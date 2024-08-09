ec37f78671c3e26d4b75ed3d239505b89a7756f4
akash <akashdinanathyadav321@gmail.com>
2024-08-09 23:22:34 +0530

    "Refactored Cloudinary configuration and upload functionality in usercontroller.js and cloudinary.js"

--- backend/util/cloudinary.js
+++ backend/util/cloudinary.js
@@ -1,8 +1,4 @@
 import { v2 as cloudinary } from 'cloudinary';
-// import { Console } from 'console';
-// import fs from 'fs';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import streamifier from 'streamifier';
 
 // Configuration
 cloudinary.config({
@@ -11,18 +7,9 @@ cloudinary.config({
   api_secret: process.env.API_SECRET // Hardcoded for now, ensure to secure this in production
 });
 
-const uploadOnCloudinary = async (buffer) => {
-  return new Promise((resolve, reject) => {
-    const uploadStream = cloudinary.uploader.upload_stream((error, result) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(result);
-      }
-    });
+const uploadOnCloudinary = async (path) => {
 
-    streamifier.createReadStream(buffer).pipe(uploadStream);
-  });
+  
 };
 
 export default uploadOnCloudinary;
