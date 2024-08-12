293729fa687a8f9a14bc5f241c6144cdc69fa722
akash <akashdinanathyadav321@gmail.com>
2024-08-12 18:51:13 +0530

    "Update CORS origin from 'http://localhost:5173' to 'https://slygallery.vercel.app'"

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ dotenv.config()
 const app = express()
 app.use(cookieParser())
 app.use(cors({
-  origin: 'http://localhost:5173', // Don't use wildcard '*' instead specify frontend url
+  origin: 'https://slygallery.vercel.app', // Don't use wildcard '*' instead specify frontend url
   credentials: true
 }))
 
