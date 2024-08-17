2f42bacb6b5effb79575eefa03a705c6be5267e3
akash <akashdinanathyadav321@gmail.com>
2024-08-17 15:45:41 +0530

    "Updated SkeletonLoader component and added animation to tailwind.config.js"

--- Frontend/src/loaders/homelosder.jsx
+++ Frontend/src/loaders/homelosder.jsx
@@ -1,9 +1,12 @@
 const SkeletonLoader = () => {
     return (
-      <div className="bg-white animate-pulse rounded-lg overflow-hidden shadow-md">
-        <div className="w-full h-80 bg-gray-200 animate-pulse"></div>
-       
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="relative w-24 h-24">
+        <div className="absolute inset-0 border-4 border-t-4 border-t-blue-500 rounded-full animate-spin"></div>
+        <div className="absolute inset-0 border-4 border-b-4 border-b-red-500 rounded-full animate-spin-reverse"></div>
+        <div className="absolute inset-0 w-16 h-16 bg-white rounded-full"></div>
       </div>
+    </div>
     );
   };
   export default SkeletonLoader;
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,8 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-reverse': 'spin-reverse 1.5s linear infinite',
+      },
+      keyframes: {
+        'spin-reverse': {
+          from: { transform: 'rotate(0deg)' },
+          to: { transform: 'rotate(-360deg)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
-
