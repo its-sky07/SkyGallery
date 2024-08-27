a6b8580e4c6fa411590d0f0be6ad2bfb9072372b
akash <akashdinanathyadav321@gmail.com>
2024-08-27 20:52:06 +0530

    "Updated Firebase configuration with new API key, auth domain, project ID, storage bucket, messaging sender ID, app ID, and measurement ID."

--- Frontend/src/Firebass/Firebass.js
+++ Frontend/src/Firebass/Firebass.js
@@ -1,17 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBLdklReru0DGEbsyWPYSDJPmblW5wk48o",
-  authDomain: "skyshop-654bf.firebaseapp.com",
-  projectId: "skyshop-654bf",
-  storageBucket: "skyshop-654bf.appspot.com",
-  messagingSenderId: "14891250792",
-  appId: "1:14891250792:web:9225aed96d6ac85b5177dd"
+  apiKey: "AIzaSyC92I90LGCChUxxlADqk1JCOcrhEh7h03I",
+  authDomain: "skygallery-15361.firebaseapp.com",
+  projectId: "skygallery-15361",
+  storageBucket: "skygallery-15361.appspot.com",
+  messagingSenderId: "29403101732",
+  appId: "1:29403101732:web:352edde441e36fc9a976b1",
+  measurementId: "G-X1WSJ45DZ5"
 };
 
 // Initialize Firebase
