454168f3428eef12811bbe5030f3571d158784ec
akash <akashdinanathyadav321@gmail.com>
2024-09-03 20:48:49 +0530

    "Removed lazy loading for components and updated Home component's loading state HTML"

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -47,14 +47,17 @@ function Home() {
     }
 
 
-    if (loading) return  <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <p> please wait.....</p> 
+    if (loading) return <>
+    <p> please wait.....</p> 
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      
     <div className="relative w-24 h-24">
       <div className="absolute inset-0 border-4 border-t-4 border-t-blue-500 rounded-full animate-spin"></div>
       <div className="absolute inset-0 border-4 border-b-4 border-b-red-500 rounded-full animate-spin-reverse"></div>
       <div className="absolute inset-0 w-16 h-16 bg-white rounded-full"></div>
     </div>
   </div>
+  </>
 
     return (
         <>
