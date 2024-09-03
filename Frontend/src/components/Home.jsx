3c029cbf9dd7da4c4289a1c974eac0e646daf47c
akash <akashdinanathyadav321@gmail.com>
2024-09-03 20:50:58 +0530

    "Moved 'please wait.....' paragraph inside a div element and added a line break."

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -48,9 +48,9 @@ function Home() {
 
 
     if (loading) return <>
-    <p> please wait.....</p> 
+   
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      
+    <p> please wait.....</p> <br />
     <div className="relative w-24 h-24">
       <div className="absolute inset-0 border-4 border-t-4 border-t-blue-500 rounded-full animate-spin"></div>
       <div className="absolute inset-0 border-4 border-b-4 border-b-red-500 rounded-full animate-spin-reverse"></div>
